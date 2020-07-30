eaaf9ce4350efc028f607cb3abd6a02222ccbf14
lichengsheng <lichengsheng@ppdai.com>
2020-07-30 11:02:59 +0800

    更新sql

--- doc/mq_basic.sql
+++ doc/mq_basic.sql
@@ -1,25 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost
-Source Server Version : 50717
-Source Host           : localhost:3306
-Source Database       : mq_basic
-
-Target Server Type    : MYSQL
-Target Server Version : 50717
-File Encoding         : 65001
-
-Date: 2019-10-15 15:15:45
-*/
-create database mq_basic;
-use mq_basic;
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for audit_log
--- ----------------------------
-DROP TABLE IF EXISTS `audit_log`;
 CREATE TABLE `audit_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `tb_name` varchar(100) NOT NULL COMMENT '名称',
@@ -34,10 +12,6 @@ CREATE TABLE `audit_log` (
   KEY `tbname_refid_idx` (`tb_name`,`ref_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='主题';
 
--- ----------------------------
--- Table structure for consumer
--- ----------------------------
-DROP TABLE IF EXISTS `consumer`;
 CREATE TABLE `consumer` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `ip` varchar(45) NOT NULL,
@@ -54,12 +28,8 @@ CREATE TABLE `consumer` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`name`),
   KEY `consumer_name_index` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订阅者信息';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='订阅者信息';
 
--- ----------------------------
--- Table structure for consumer_group
--- ----------------------------
-DROP TABLE IF EXISTS `consumer_group`;
 CREATE TABLE `consumer_group` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL COMMENT '订阅者集合名称，唯一',
@@ -88,16 +58,12 @@ CREATE TABLE `consumer_group` (
   `meta_update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '元数据更新时间',
   `mode` int(11) NOT NULL DEFAULT '1' COMMENT '1，为集群模式，2，为广播模式,3，为代理模式',
   `origin_name` varchar(50) DEFAULT NULL COMMENT '原始的消费者组名',
-  `sub_env` varchar(45) NOT NULL DEFAULT 'default'  COMMENT '子环境名称',
-  `push_flag` int(11) NOT NULL DEFAULT '0' COMMENT '1，表示实时推送，0，表示非实时推送',
+  `sub_env` varchar(45) NOT NULL DEFAULT 'default',
+  `push_flag` int(11) NOT NULL DEFAULT '0' COMMENT '1,表示实时推送，0，表示非实时推送'
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_index` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='消费者组元数据表';
 
--- ----------------------------
--- Table structure for consumer_group_consumer
--- ----------------------------
-DROP TABLE IF EXISTS `consumer_group_consumer`;
 CREATE TABLE `consumer_group_consumer` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `consumer_id` bigint(20) NOT NULL COMMENT '消费者Id',
@@ -112,12 +78,8 @@ CREATE TABLE `consumer_group_consumer` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `consumer_group_consumer_uq` (`consumer_id`,`consumer_group_id`),
   KEY `consumer_group_id_idx` (`consumer_group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订阅者信息';
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='订阅者信息';
 
--- ----------------------------
--- Table structure for consumer_group_topic
--- ----------------------------
-DROP TABLE IF EXISTS `consumer_group_topic`;
 CREATE TABLE `consumer_group_topic` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `consumer_group_id` bigint(20) NOT NULL,
@@ -130,7 +92,7 @@ CREATE TABLE `consumer_group_topic` (
   `thread_size` int(11) DEFAULT '10',
   `max_lag` int(11) DEFAULT '1000' COMMENT '可以自定义此topic下topic 告警条数',
   `tag` varchar(100) DEFAULT NULL COMMENT '用来做消息tag 过滤,规则是只要包含在消息体tag中就算符合',
-  `delay_process_time` int(11) DEFAULT '0' COMMENT '默认为0，秒为单位。延迟处理时间，相对于发送时间的延迟，例如希望发送一条消息后10秒后被订阅，就需要设置该参数为10000。',
+  `delay_process_time` int(11) DEFAULT '0' COMMENT '默认为0，毫秒为单位。延迟处理时间，相对于发送时间的延迟，例如希望发送一条消息后10秒后被订阅，就需要设置该参数为10000。',
   `pull_batch_size` int(11) DEFAULT '50' COMMENT '批量拉取条数',
   `consumer_batch_size` int(11) DEFAULT '1' COMMENT '批量消费条数',
   `max_pull_time` int(11) DEFAULT '5' COMMENT '最大拉取等待时间，单位是秒默认5秒，最小值1秒',
@@ -146,10 +108,6 @@ CREATE TABLE `consumer_group_topic` (
   UNIQUE KEY `group_topic_id_uq` (`consumer_group_id`,`topic_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for db_node
--- ----------------------------
-DROP TABLE IF EXISTS `db_node`;
 CREATE TABLE `db_node` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `ip` varchar(45) DEFAULT NULL COMMENT '表示数据对应的ip',
@@ -171,34 +129,16 @@ CREATE TABLE `db_node` (
   `update_by` varchar(100) DEFAULT NULL COMMENT '操作人',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `is_active` tinyint(4) NOT NULL DEFAULT '1' COMMENT '逻辑删除',
+  `ip_01` varchar(45) DEFAULT NULL,
+  `port_01` smallint(11) DEFAULT NULL,
+  `ip_bak_01` varchar(45) DEFAULT NULL,
+  `port_bak_01` smallint(11) DEFAULT NULL,
+  `con_str_01` varchar(500) DEFAULT NULL COMMENT '数据库账户名-备份',
   PRIMARY KEY (`id`),
   UNIQUE KEY `con_str_UNIQUE` (`con_str`),
   KEY `update_time_idx` (`update_time`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='数据库节点';
 
--- ----------------------------
--- Table structure for dic
--- ----------------------------
-DROP TABLE IF EXISTS `dic`;
-CREATE TABLE `dic` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `key1` varchar(50) NOT NULL COMMENT '键',
-  `value1` varchar(100) NOT NULL COMMENT '值',
-  `remark` varchar(100) DEFAULT NULL COMMENT '备注',
-  `insert_by` varchar(100) DEFAULT NULL COMMENT '操作人',
-  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-  `update_by` varchar(100) DEFAULT NULL COMMENT '操作人',
-  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-  `is_active` tinyint(4) NOT NULL DEFAULT '0' COMMENT '逻辑删除',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `key_index` (`key1`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
--- ----------------------------
--- Table structure for message_01
--- ----------------------------
-DROP TABLE IF EXISTS `message_01`;
 CREATE TABLE `message_01` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `busi_id` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '业务id',
@@ -213,10 +153,6 @@ CREATE TABLE `message_01` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='消息';
 
--- ----------------------------
--- Table structure for mq_lock
--- ----------------------------
-DROP TABLE IF EXISTS `mq_lock`;
 CREATE TABLE `mq_lock` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `ip` varchar(45) DEFAULT NULL,
@@ -233,10 +169,6 @@ CREATE TABLE `mq_lock` (
   KEY `key_idx` (`key1`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for notify_message
--- ----------------------------
-DROP TABLE IF EXISTS `notify_message`;
 CREATE TABLE `notify_message` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `consumer_group_id` bigint(20) NOT NULL,
@@ -249,10 +181,6 @@ CREATE TABLE `notify_message` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用来记录需要重平衡的consumer_group';
 
--- ----------------------------
--- Table structure for notify_message_stat
--- ----------------------------
-DROP TABLE IF EXISTS `notify_message_stat`;
 CREATE TABLE `notify_message_stat` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `key1` varchar(45) NOT NULL COMMENT 'notifymessage标识，防止重复插入',
@@ -266,10 +194,6 @@ CREATE TABLE `notify_message_stat` (
   UNIQUE KEY `key1_UNIQUE` (`key1`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='用来记录需要重平衡的消息id';
 
--- ----------------------------
--- Table structure for queue
--- ----------------------------
-DROP TABLE IF EXISTS `queue`;
 CREATE TABLE `queue` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `topic_id` bigint(20) NOT NULL DEFAULT '0' COMMENT 'topic id',
@@ -293,10 +217,6 @@ CREATE TABLE `queue` (
   KEY `topic_id_idx` (`topic_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='分区';
 
--- ----------------------------
--- Table structure for queue_offset
--- ----------------------------
-DROP TABLE IF EXISTS `queue_offset`;
 CREATE TABLE `queue_offset` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `consumer_group_id` bigint(20) NOT NULL DEFAULT '0' COMMENT '订阅者组id',
@@ -321,17 +241,13 @@ CREATE TABLE `queue_offset` (
   `meta_update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '元数据更新时间',
   `origin_consumer_group_name` varchar(50) DEFAULT NULL COMMENT '原始的消费者组名',
   `consumer_group_mode` int(11) NOT NULL DEFAULT '1' COMMENT '1，为集群模式，2，为广播模式,3，为代理模式',
-  `sub_env` varchar(45) NOT NULL DEFAULT 'default' COMMENT '子环境名称',
+  `sub_env` varchar(45) NOT NULL DEFAULT 'default',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uq_group_id_topic_id` (`consumer_group_id`,`topic_id`,`queue_id`),
   KEY `consumer_group_id_idx` (`consumer_group_id`),
   KEY `consumer_idx` (`consumer_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='客户端的topic信息';
 
--- ----------------------------
--- Table structure for server
--- ----------------------------
-DROP TABLE IF EXISTS `server`;
 CREATE TABLE `server` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `ip` varchar(45) DEFAULT NULL,
@@ -345,15 +261,11 @@ CREATE TABLE `server` (
   `update_by` varchar(100) DEFAULT NULL COMMENT '操作人',
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
   `is_active` tinyint(4) NOT NULL DEFAULT '0' COMMENT '逻辑删除',
-  PRIMARY KEY (`id`), 
+  PRIMARY KEY (`id`),
   UNIQUE KEY `ip_port_uq` (`ip`,`port`),
   KEY `ip_port_idx` (`ip`,`port`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Table structure for topic
--- ----------------------------
-DROP TABLE IF EXISTS `topic`;
 CREATE TABLE `topic` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL COMMENT '名称',
@@ -385,3 +297,17 @@ CREATE TABLE `topic` (
   KEY `name_idx` (`name`),
   KEY `update_time_idex` (`update_time`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='主题';
+
+CREATE TABLE `dic` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `key1` varchar(50) NOT NULL COMMENT '键',
+  `value1` varchar(100) NOT NULL COMMENT '值',
+  `remark` varchar(100) DEFAULT NULL COMMENT '备注',
+  `insert_by` varchar(100) DEFAULT NULL COMMENT '操作人',
+  `insert_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_by` varchar(100) DEFAULT NULL COMMENT '操作人',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `is_active` tinyint(4) NOT NULL DEFAULT '0' COMMENT '逻辑删除',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `key_index` (`key1`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
