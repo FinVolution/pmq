e885ac5e2061782bb17fb3bc589924b7ffb45cf0
lichengsheng <lichengsheng@ppdai.com>
2020-08-28 15:25:52 +0800

    优化反射获取对象属性值

--- mq-biz/src/main/java/com/ppdai/infrastructure/mq/biz/common/util/SpringUtil.java
+++ mq-biz/src/main/java/com/ppdai/infrastructure/mq/biz/common/util/SpringUtil.java
@@ -1,12 +1,15 @@
 package com.ppdai.infrastructure.mq.biz.common.util;
 
-import java.lang.reflect.Field;
-import java.util.Map;
-
+import org.springframework.aop.TargetSource;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Map;
 
 @Component
 public class SpringUtil implements ApplicationContextAware {
@@ -86,13 +89,43 @@ public class SpringUtil implements ApplicationContextAware {
 
 	private static Object getValue(String fieldName, Object object) {
 		try {
-			Field field = object.getClass().getDeclaredField(fieldName);
+			Field field = ReflectionUtils.findField(object.getClass(),fieldName);
 			field.setAccessible(true);
-			Object rs = field.get(object);
+			Object rs = field.get(extractTargetObject(object));
 			field.setAccessible(false);
 			return rs;
 		} catch (Exception e) {
 			return null;
 		}
 	}
+
+	private static Object extractTargetObject(Object proxied) {
+		try {
+			return findSpringTargetSource(proxied).getTarget();
+		} catch (Exception e) {
+			return  proxied;
+		}
+	}
+
+	private static TargetSource findSpringTargetSource(Object proxied) {
+		Method[] methods = proxied.getClass().getDeclaredMethods();
+		Method targetSourceMethod = findTargetSourceMethod(methods);
+		targetSourceMethod.setAccessible(true);
+		try {
+			return (TargetSource)targetSourceMethod.invoke(proxied);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Method findTargetSourceMethod(Method[] methods) {
+		for (Method method : methods) {
+			if (method.getName().endsWith("getTargetSource")) {
+				return method;
+			}
+		}
+		throw new IllegalStateException(
+				"Could not find target source method on proxied object []");
+	}
+
 }
